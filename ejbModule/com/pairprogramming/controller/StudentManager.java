22d24fd53ea1059f975a3a3090806dec1e66b31f
Adamson Jeremiah <jaletechs@gmail.com>
2017-02-05 08:52:06 +0100

    added files

---
+++ ejbModule/com/pairprogramming/controller/StudentManager.java
@@ -0,0 +1,8 @@
+package com.pairprogramming.controller;
+
+import javax.ejb.Local;
+
+@Local
+public interface StudentManager {
+
+}
---
+++ ejbModule/com/pairprogramming/controller/StudentManagerBean.java
@@ -0,0 +1,18 @@
+package com.pairprogramming.controller;
+
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class StudentManagerBean
+ */
+@Stateless
+public class StudentManagerBean implements StudentManager {
+
+    /**
+     * Default constructor. 
+     */
+    public StudentManagerBean() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
